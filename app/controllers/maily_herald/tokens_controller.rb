94d3ba57920f2b1ce08be07e9e262ea4ebf89a94
Łukasz Jachymczyk <lukasz@sology.eu>
2013-09-25 12:59:59 +0200

    Fix subscription token lookup

--- app/controllers/maily_herald/tokens_controller.rb
+++ app/controllers/maily_herald/tokens_controller.rb
@@ -1,12 +1,15 @@
 module MailyHerald
   class TokensController < MailyHerald::ApplicationController
     def get
-      @subscription = Subscription.find_by_token(params[:token])
-      if @subscription.target.token_action == :custom
+      @subscription = MailyHerald::Subscription.find_by_token(params[:token])
+
+      if @subscription && @subscription.target.token_action == :custom
         @subscription.target.token_custom_action.call(self, @subscription)
-      else
+      elsif @subscription
         @subscription.deactivate!
-        redirect_to MailyHerald.token_redirect.call(@subscription), :notice => t('subscription.deactivated')
+        redirect_to MailyHerald.token_redirect.call(@subscription), :notice => t('maily_herald.subscription.deactivated')
+      else
+        redirect_to "/", :notice => t('maily_herald.subscription.undefined_token')
       end
     end
   end
