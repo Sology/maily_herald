acdaec1790e42f1b6d7eb5e2d1bc8a02e53956b2
Łukasz Jachymczyk <lukasz@sology.eu>
2017-10-26 11:55:56 +0200

    DB Migration compatibility.

--- db/migrate/20150205120443_create_maily_herald_tables.rb
+++ db/migrate/20150205120443_create_maily_herald_tables.rb
@@ -1,4 +1,4 @@
-class CreateMailyHeraldTables < ActiveRecord::Migration
+class CreateMailyHeraldTables < ActiveRecord::Migration[4.2]
   def change
     create_table :maily_herald_dispatches do |t|
       t.string            :type,                                        null: false
--- db/migrate/20171019105242_remove_override_subscription_from_maily_herald_dispatches.rb
+++ db/migrate/20171019105242_remove_override_subscription_from_maily_herald_dispatches.rb
@@ -1,4 +1,4 @@
-class RemoveOverrideSubscriptionFromMailyHeraldDispatches < ActiveRecord::Migration[5.1]
+class RemoveOverrideSubscriptionFromMailyHeraldDispatches < ActiveRecord::Migration[4.2]
   def change
     remove_column :maily_herald_dispatches, :override_subscription
   end
